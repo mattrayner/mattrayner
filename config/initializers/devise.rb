70dc321b9b4c0d93f98f3d1429d0954e4053780d
Matt Rayner <mattrayner1@gmail.com>
2015-03-08 12:00:07 +0000

    [#2] Styled login flow * Created an Admin layout * Generated devise views * Added titles to the **full** devise flow * Updated routes to give a nicer devise url set * Set 'admin' layout for devise controllers and admin controllers * Added bootstrap to the Gemfile * Modified devise views to fit with the bootstrap styles * Added Travis CI config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,13 +4,13 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '9b3ac7c7107652005cd2395c7917b20004d8ef87eb5ff93c21e5ebf5a897a11e52d3515fbd2044944c9570f529c31f27835d5659e71346c03de62fe1eb8f7955'
+  config.secret_key = '9b3ac7c7107652005cd2395c7917b20004d8ef87eb5ff93c21e5ebf5a897a11e52d3515fbd2044944c9570f529c31f27835d5659e71346c03de62fe1eb8f7955'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'no-reply@mattrayner.co.uk'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
