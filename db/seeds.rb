9e3cfcfee93dd54f2ee10d972765ef19cac78b80
Matt Rayner <mattrayner1@gmail.com>
2015-03-13 00:00:25 +0000

    [#24] Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Seeding data for MattRayner.co.uk...'
+
+if Rails.env.production?
+  puts "\nWHOOPS! You're not allowed to seed like this on production! That's hella dangerous."
+  raise 'Seed Error: You are not allowed to seed on production'
+end
+
+def create_user(email:, password:)
+  puts "\t\t- #{email}"
+
+  user = User.find_or_initialize_by(email: email)
+
+  puts( user.new_record? ? "\t\t\t- Creating..." : "\t\t\t- Already Exists - Updating..." )
+
+  user.password              = password
+  user.password_confirmation = password
+  user.save!
+end
+
+def create_project(title:, intro:, body:)
+  puts "\t\t- #{title}"
+
+  project = Project.find_or_initialize_by(title: title)
+
+  puts( project.new_record? ? "\t\t\t- Creating..." : "\t\t\t- Already Exists - Updating..." )
+
+  project.intro = intro
+  project.body  = body
+  project.save!
+end
+
+puts "\t- Seeding users"
+create_user(email: 'admin@example.com', password: 'mj$jCh@%mvhhNbpn!IXKZPWD66Fs0LOkfkuw^VOtwU@x&@57KTAmqjh*w2ZGa%FJI8jl0lws*r2rcsE2&6Jq$k5jEPBNd^M$VB6S')
+
+puts "\t- Seeding projects"
+create_project(title: 'Give Cat', intro: 'News websites getting you **down**? Can\'t face *another* look at 9gag? Or are you just fed up of looking at your friend\'s ***ugly***{: style="color: red"} faces?', body: 'Inspired by [Hey Girl](http://heygirl.io/), GiveCat was a side-project spawned from love and challenge.
+
+Conceived in a university library after too many red bulls, I wanted to create a cute bookmark powered by an API and content management system.
+
+##But why?
+
+That\'s a good question... I noticed one *major* (to me anyway) flaw with Hey Girl. The image selection.
+Hard coded into Hey Girl is an array of 20 something images that the bookmarklet selects at random and uses. This seemed a but too small for me. I wanted to **never** run out of ossum cute things.')
